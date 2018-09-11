91c603001e91d1a03d1e365bc93148cc8fa1fd99
Thanh Danh <thanhdanh2604@gmail.com>
2018-09-11 16:25:55 +0700

    abc

--- index.php
+++ index.php
@@ -1,20 +1,15 @@
 <?php
-/*+**********************************************************************************
- * The contents of this file are subject to the vtiger CRM Public License Version 1.1
- * ("License"); You may not use this file except in compliance with the License
- * The Original Code is:  vtiger CRM Open Source
- * The Initial Developer of the Original Code is vtiger.
- * Portions created by vtiger are Copyright (C) vtiger.
- * All Rights Reserved.
- ************************************************************************************/
 
-//Overrides GetRelatedList : used to get related query
-//TODO : Eliminate below hacking solution
-include_once './config.php';
-include_once 'include/Webservices/Relation.php';
-
-include_once 'vtlib/Vtiger/Module.php';
-include_once 'includes/main/WebUI.php';
-
-$webUI = new Vtiger_WebUI();
-$webUI->process(new Vtiger_Request($_REQUEST, $_REQUEST));
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Test git</title>
+	<link rel="stylesheet" href="">
+</head>
+<body>
+	
+</body>
+</html>
